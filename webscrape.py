d0035868a87b999d7d995f5c8744fe83afd08918
Soumya Ranjan Mishra <75298645+balaramdevil@users.noreply.github.com>
2023-06-03 01:14:59 +0530

    Update webscrape.py
    Adding the functionality to change the pdf files into txt and saving them as well

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import PyPDF2
 
 url = "https://www.federalreserve.gov/monetarypolicy/publications/mpr_default.htm"
 
@@ -19,6 +20,14 @@ for link in li:
 		pdf = open("FOMC_Press "+str(i)+".pdf", 'wb')
 		pdf.write(response.content)
 		pdf.close()
+		pdf_file = open("FOMC_Press "+str(i)+".pdf", 'rb')
+		pdf_reader = PyPDF2.PdfReader(pdf_file)
+		txt_file = open("FOMC_Press "+str(i)+".txt", 'w',encoding="utf-8")
+		for page_num in range(len(pdf_reader.pages)):
+			page = pdf_reader.pages[page_num]
+			txt_file.write(page.extract_text())
+		pdf_file.close()
+		txt_file.close()
 		print("File ", i, " downloaded")
 
 print("All PDF files downloaded")
