b2268d5db1c65a0a0d0899c657b41e7ff86f7084
prayutjain <prayutjain@gmail.com>
2023-07-09 19:08:00 -0500

    Create prepdata.py

---
+++ prepdata.py
@@ -0,0 +1,11 @@
+## Script for scraping all FOMC data into 1 dataframe
+
+from util.utilities_v1 import FedReserveData
+
+chrome_path = r"C:/Users/Prayut Jain/AppData/Local/Google/Chrome/User Data/Default"
+
+fed_data = FedReserveData(chrome_path)
+df = fed_data.get_data(year_range = (1994,2023))
+
+df.to_pickle("../data/raw_data.pkl")
+# clean_df.to_pickle("../data/clean_data.pkl")
